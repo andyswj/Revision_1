6a171ac23dfde49fd48c7275a4d5c02b978762db
andyswj <e0696682@u.nus.edu>
2022-03-07 14:41:27 +0800

    new

---
+++ Revision_1/src/main/java/edu/nus/iss/Revision_1/Revision1Application.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.Revision_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Revision1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Revision1Application.class, args);
+	}
+
+}
---
+++ Revision_1/src/test/java/edu/nus/iss/Revision_1/Revision1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nus.iss.Revision_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Revision1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
